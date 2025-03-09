2d5ceda4d5d8096e417222566175847da35194f2
Shaun Mia <84870531+shaun-mia@users.noreply.github.com>
2025-03-10 00:17:08 +0600

    basic_py

---
+++ python/recoarded_class/basic_syntax.py
@@ -0,0 +1,13 @@
+#Statement 1
+print("Hello Word")
+#Statement 2
+X = 20
+Y = 30
+print( X + Y )
+#Statement 3
+print(X)
+
+A = int(input("Enter a Number"))
+B = int(input("Enter another Number"))
+
+print(A + B)
